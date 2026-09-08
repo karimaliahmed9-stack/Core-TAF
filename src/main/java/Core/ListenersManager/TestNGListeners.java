186c46028135975bf2c050d3ec34c521f156650b
Karim Ali QC Engineer <karimaliahmed9@gmail.com>
2026-09-08 18:38:57 +0300

    Updated configer listeners class

--- src/main/java/Core/ListenersManager/TestNGListeners.java
+++ src/main/java/Core/ListenersManager/TestNGListeners.java
@@ -1,34 +1,61 @@
 package Core.ListenersManager;
 
+import Core.AttachementsManager.ScreenShootsManager;
+import Core.DataReaderManager.PropertyReader;
+import Core.FileUtilsManager.FileUtil;
+import Core.LogManager.LogManager;
 import org.testng.*;
 
-public class TestNGListeners implements IExecutionListener , IInvokedMethodListener, ITestListener {
-    public void onExecutionStart() {
-        // not implemented
+import java.io.File;
+
+public class TestNGListeners implements IExecutionListener, IInvokedMethodListener, ITestListener {
+    public TestNGListeners() {
+        LogManager.Info("TestNGListeners Initialized : .....");
     }
 
+    public void onExecutionStart() {
+        LogManager.Info("Test Execution Started");
+        CleaningTestOutputDirectory();
+        LogManager.Info("Cleaning directors: ");
+        CreatingDirectory();
+        LogManager.Info("Creating directors: ...");
+        //كدا نا بنادي علي الفايلات من الريسورسز
+        PropertyReader.loadproperties();
+        LogManager.Info("Properties loaded");
+    }
 
     public void onExecutionFinish() {
-        // not implemented
+        LogManager.Info("Starting Test Execution Finished: ... ");
+        LogManager.Info("Test Execution Finished: ... ");
     }
+
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
-        // not implemented
+
     }
 
     public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
-        // not implemented
-    }
-    public void onTestStart(ITestResult result) {
-        // not implemented
-    }
 
+    }
 
     public void onTestSuccess(ITestResult result) {
-        // not implemented
+        LogManager.Info("Test Case Passed: " + result.getName() + "Passed");
+    }
+
+    public void onTestFailure(ITestResult result) {
+        LogManager.Info("Test Case Failed: " + result.getName() + "Failed");
+    }
+
+    public void onTestSkipped(ITestResult result) {
+        LogManager.Info("Test Case Skipped: " + result.getName() + "Skipped");
     }
 
 
-    public void onTestFailure(ITestResult result) {
-        // not implemented
+    public static void CleaningTestOutputDirectory() {
+        FileUtil.cleaningDirectory(new File(ScreenShootsManager.screenshoot_path));
+        FileUtil.cleaningDirectory(new File(LogManager.LOG_PATH));
+    }
+
+    public void CreatingDirectory() {
+        FileUtil.CreatingDirectory(ScreenShootsManager.screenshoot_path);
     }
 }
