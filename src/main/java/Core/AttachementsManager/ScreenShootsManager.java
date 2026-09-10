a5af5e2a26503d31f5db312fa8e71a072fecd52f
Karim Ali.QC <karimaliahmed9@gmail.com>
2026-09-10 14:51:53 +0300

    Update the Path screenshots

--- src/main/java/Core/AttachementsManager/ScreenShootsManager.java
+++ src/main/java/Core/AttachementsManager/ScreenShootsManager.java
@@ -11,7 +11,7 @@ import java.io.File;
 
 public class ScreenShootsManager {
 
-    public static final String screenshoot_path = "Test-Output/ScreenShoot/";
+    public static final String screenshoot_path = "Test-Output/ScreenShoots/";
 
 
     //Take full page screen shoot
